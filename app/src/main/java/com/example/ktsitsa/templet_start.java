e772d7c576cf38c348848b3c1a64d9d3a65f8744
or trabelsi <ortrsa@gmail.com>
2021-11-22 14:34:57 +0200

    first commit

--- app/src/main/java/com/example/ktsitsa/templet_start.java
+++ app/src/main/java/com/example/ktsitsa/templet_start.java
@@ -13,7 +13,7 @@ public class templet_start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.templet);
-//        ofir hgfhgf
+//        ofir 111111111
     }
 
     public void HomeBtnClick(View view) {
