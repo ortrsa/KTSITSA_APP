33f7c5158e695c31431edf1c6ae3991d0056012d
ofir-magen <66851296+ofir-magen@users.noreply.github.com>
2021-11-22 19:00:03 +0200

    add to resapis

--- app/src/main/java/com/example/ktsitsa/AddRecipes.java
+++ app/src/main/java/com/example/ktsitsa/AddRecipes.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddRecipes extends AppCompatActivity {
 
@@ -17,6 +22,28 @@ public class AddRecipes extends AppCompatActivity {
     public void HomeBtnClick(View view) {
         Intent intent = new Intent(AddRecipes.this, MainActivity.class);
         startActivity(intent);
+
+    }
+
+    public void add(View view) {
+
+        EditText TxtTitle= (EditText)findViewById(R.id.TxtTitle);
+        String Title =TxtTitle.getText().toString();
+
+        EditText TxtIngredients= (EditText)findViewById(R.id.TxtIngredients);
+        String Ingredients =TxtTitle.getText().toString();
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Title");
+        myRef.setValue(Title.toString());
+
+        DatabaseReference myRefX = database.getReference("Ingredients");
+        myRef.setValue(Ingredients.toString());
+
+
+
+
 
     }
 }
