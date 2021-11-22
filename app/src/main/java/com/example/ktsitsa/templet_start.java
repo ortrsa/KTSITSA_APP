986111e3d3388cb1abed7528ff1185fed52435bb
ofir-magen <66851296+ofir-magen@users.noreply.github.com>
2021-11-22 14:32:52 +0200

    try1

--- app/src/main/java/com/example/ktsitsa/templet_start.java
+++ app/src/main/java/com/example/ktsitsa/templet_start.java
@@ -13,7 +13,7 @@ public class templet_start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.templet);
-//        ofir hgfhgf
+//        ofir iuiuyiuyiuyi
     }
 
     public void HomeBtnClick(View view) {
