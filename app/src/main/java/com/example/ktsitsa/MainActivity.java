d8fd8ac74a4da677ef2dad8e26dbf174af3916b8
uriyashemla <uriyashem1994@gmail.com>
2021-12-26 15:17:34 +0200

    search

--- app/src/main/java/com/example/ktsitsa/MainActivity.java
+++ app/src/main/java/com/example/ktsitsa/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this, Search_by_Ingredient.class);
+                    Intent intent = new Intent(MainActivity.this, Search_Options.class);
                     intent.putExtra("isAdmin",IsAdmin);
                     startActivity(intent);
             }
