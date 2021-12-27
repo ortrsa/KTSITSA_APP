6b08c614d1cd2bcfb0da78f4532cfbaef35f898d
Nadav-Eps <nadavepstein1@gmail.com>
2021-12-27 15:43:40 +0200

    notification

---
+++ app/src/main/java/com/example/ktsitsa/PushNotificationService.java
@@ -0,0 +1,8 @@
+package com.example.ktsitsa;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class PushNotificationService extends FirebaseMessagingService {
+
+}
