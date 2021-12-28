4e82649645e9b55907bff03e71d50c391717fef0
or trabelsi <ortrsa@gmail.com>
2021-12-28 19:36:44 +0200

    notification

--- app/src/main/java/com/example/ktsitsa/send_notification.java
+++ app/src/main/java/com/example/ktsitsa/send_notification.java
@@ -1,12 +1,16 @@
 package com.example.ktsitsa;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,10 +23,11 @@ import java.util.ArrayList;
 public class send_notification extends AppCompatActivity {
 
 
-    private Button notify_button;
+    private Button notify_button, custom_notify_btn;
     private DatabaseReference database;
     private ArrayList<String> RecList;
-    private String RandRec, RandTitle, RandMess;
+    private String RandRec, RandTitle, RandMess, Stitle, Smessage;
+    private EditText title, message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,12 +76,68 @@ public class send_notification extends AppCompatActivity {
             public void onClick(View v) {
                 if(RecList.size()>0) {
 
-                    int index = (int) (Math.random() * RecList.size());
-                    RandRec = RecList.get(index);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(send_notification.this);
+                    builder.setTitle(" שליחת התראה אוטומטית            ");
+                    builder.setCancelable(false);
+                    builder.setMessage("שלחיחת התראה אוטומטית לא תתיחס לשדות המותאמים אישית ותשלח מתכון מומלץ רנדומלי!").setPositiveButton("שלח!", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            int index = (int) (Math.random() * RecList.size());
+                            RandRec = RecList.get(index);
+                            FcmNotificationsSender fsend = new FcmNotificationsSender("/topics/all", "המלצת היום בקציצה!", "נראה לנו שאנחנו יודעים מה בא לכם היום\n " + RandRec + " מעולה" +"\nעכשיו במומלצים", getApplicationContext(), send_notification.this);
+                            fsend.SendNotifications();
+                            Toast.makeText(send_notification.this, "פורסם!", Toast.LENGTH_SHORT).show();
+                        }
+                    }).setNegativeButton("ביטול", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    builder.show();
 
-                    FcmNotificationsSender fsend = new FcmNotificationsSender("/topics/all", "המלצת היום בקציצה!", "נראה לנו שאנחנו יודעים מה בא לכם היום\n " + RandRec + " מעולה" +"\nעכשיו במומלצים", getApplicationContext(), send_notification.this);
-                    fsend.SendNotifications();
+
+                }else{
+                    Toast.makeText(send_notification.this, "אין מתכונים מומלצים!", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        custom_notify_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Stitle = title.getText().toString();
+                Smessage = message.getText().toString();
+
+                if(!Stitle.isEmpty() && !Smessage.isEmpty()) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(send_notification.this);
+                    builder.setTitle(" שליחת התראה                ");
+                    builder.setCancelable(false);
+                    builder.setMessage("האם לשלוח התראה זו לכלל המשתמשים?").setPositiveButton("שלח!", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            FcmNotificationsSender fsend = new FcmNotificationsSender("/topics/all", Stitle, Smessage, getApplicationContext(), send_notification.this);
+                            fsend.SendNotifications();
+                            title.setText("");
+                            message.setText("");
+                            Toast.makeText(send_notification.this, "פורסם!", Toast.LENGTH_SHORT).show();
+                        }
+                    }).setNegativeButton("ביטול", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    builder.show();
+
+
+                }else{
+                    Toast.makeText(send_notification.this, "מלא את השדות כותרת ותוכן ההתראה!", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
@@ -84,7 +145,13 @@ public class send_notification extends AppCompatActivity {
     private void initData() {
         RecList = new ArrayList<>();
         notify_button = findViewById(R.id.send_notify_btn);
+        custom_notify_btn = findViewById(R.id.send_notify_btn_Custom);
         database = FirebaseDatabase.getInstance().getReference("recipes");
+        title = findViewById(R.id.TxtTitlenotify);
+        message = findViewById(R.id.txtmessnotify);
+
+
+
 
     }
 
