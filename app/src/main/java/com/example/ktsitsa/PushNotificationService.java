6638a1f80ddb6597c18d7edf01c83555f3a35974
uriyashemla <uriyashem1994@gmail.com>
2021-12-29 16:21:11 +0200

    a

--- app/src/main/java/com/example/ktsitsa/PushNotificationService.java
+++
@@ -1,8 +0,0 @@
-package com.example.ktsitsa;
-
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingService;
-
-public class PushNotificationService extends FirebaseMessagingService {
-
-}
